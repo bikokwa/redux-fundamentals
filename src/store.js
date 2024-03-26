4d396d7b9376d24ccfe9aae8ab54575cefd648a7
bikokwa <34458781+bikokwa@users.noreply.github.com>
2024-03-26 14:09:38 +0300

    write an async function middleware

--- src/store.js
+++ src/store.js
@@ -1,17 +1,35 @@
 import { applyMiddleware, createStore } from 'redux'
 import rootReducer from './reducer'
+import { client } from './api/client'
 
-const delayedMessageMiddleware = (storeAPI) => (next) => (action) => {
-  if (action.type === 'todos/todoAdded') {
-    setTimeout(() => {
-      console.log('Added a new todo: ', action.payload)
-    }, 1000)
+const asyncFunctionMiddleware = (storeAPI) => (next) => (action) => {
+  // If the "action" is actually a function instead..
+  if (typeof action === 'function') {
+    // then call the function and pass `dispatch` and `getState` as arguments
+    return action(storeAPI.dispatch, storeAPI.getState)
   }
+
+  // Otherwise, it's a normal action - send it onwards
   return next(action)
 }
 
-const middlewareEnhancer = applyMiddleware(delayedMessageMiddleware)
+const middlewareEnhancer = applyMiddleware(asyncFunctionMiddleware)
 
-const store = createStore(rootReducer, undefined, middlewareEnhancer)
+const store = createStore(rootReducer, middlewareEnhancer)
+
+// Write a function that has `dispatch` and `getState` as arguments
+const fetchSomeData = (dispatch, getState) => {
+  // Make an async HTTP request
+  client.get('todos').then((todos) => {
+    // Dispatch an action with the todos we received
+    dispatch({ type: 'todos/todosLoaded', payload: todos })
+    // Check the updated store state after dispatching
+    const allTodos = getState().todos
+    console.log('Number of todos after loading: ', allTodos.length)
+  })
+}
+
+// Pass the _function_ we wrote to `dispatch`
+store.dispatch(fetchSomeData)
 
 export default store
