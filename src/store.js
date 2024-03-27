0685f76c33778f73ec34b7772ba92a49432c02b5
bikokwa <34458781+bikokwa@users.noreply.github.com>
2024-03-27 16:48:35 +0300

    store setup

--- src/store.js
+++ src/store.js
@@ -1,10 +1,14 @@
-import { applyMiddleware, createStore } from 'redux'
-import { thunk } from 'redux-thunk'
-import { composeWithDevTools } from '@redux-devtools/extension'
-import rootReducer from './reducer'
+import { configureStore } from '@reduxjs/toolkit'
 
-const composedEnhancer = composeWithDevTools(applyMiddleware(thunk))
+import todosReducer from './features/todos/todosSlice'
+import filtersReducer from './features/filters/filtersSlice'
 
-const store = createStore(rootReducer, composedEnhancer)
+const store = configureStore({
+  reducer: {
+    // Define a top level state field named `todos`, handled by `todosReducer`
+    todos: todosReducer,
+    filters: filtersReducer,
+  },
+})
 
 export default store
