e0e10d5025ec4e11015f5b1426b81ee225e474c3
bikokwa <34458781+bikokwa@users.noreply.github.com>
2024-03-27 06:32:44 +0300

    selectors with multiple arguments

--- src/features/todos/todosSlice.js
+++ src/features/todos/todosSlice.js
@@ -1,12 +1,8 @@
 import { createSelector } from 'reselect'
 import { client } from '../../api/client'
+import { StatusFilters } from '../filters/filtersSlice'
 
 const initialState = []
-
-function nextTodoId(todos) {
-  const maxId = todos.reduce((maxId, todo) => Math.max(todo.id, maxId), -1)
-  return maxId + 1
-}
 
 export default function todosReducer(state = initialState, action) {
   switch (action.type) {
@@ -93,8 +89,39 @@ export function saveNewTodo(text) {
   }
 }
 
+// Selectors
+export const selectTodos = (state) => state.todos
+
+export const selectTodoById = (state, todoId) => {
+  return selectTodos(state).find((todo) => todo.id === todoId)
+}
+
 // Memoized selectors
-export const selectTodoIds = createSelector(
-  (state) => state.todos,
-  (todos) => todos.map((todo) => todo.id)
+export const selectTodoIds = createSelector(selectTodos, (todos) =>
+  todos.map((todo) => todo.id)
+)
+
+export const selectFilteredTodos = createSelector(
+  selectTodos,
+  (state) => state.filters,
+  (todos, filters) => {
+    const { status, colors } = filters
+    const showAllCompletions = status === StatusFilters.All
+    if (showAllCompletions && colors.length === 0) {
+      return todos
+    }
+
+    const completedStatus = status === StatusFilters.Completed
+    return todos.filter((todo) => {
+      const statusMatches =
+        showAllCompletions || todo.completed === completedStatus
+      const colorMatches = colors.length === 0 || colors.includes(todo.color)
+      return statusMatches && colorMatches
+    })
+  }
+)
+
+export const selectFilteredTodoIds = createSelector(
+  selectFilteredTodos,
+  (filteredTodos) => filteredTodos.map((todo) => todo.id)
 )
